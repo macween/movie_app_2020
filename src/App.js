eb790e1c2eeb1290268e78d454ced5482d33c27b
Chanjung Song <macween@gmail.com>
2021-11-27 12:28:37 +0900

    modified.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, pic}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={pic}  alt=""/>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/babshim/20210525042622147ztkc.jpg',
+    rating: 3.2,
+  },
+  {
+    id: 2,
+    name: '삼겹살',
+    image: 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/babshim/20210525042622147ztkc.jpg',
+    rating: 2.2,
+  },
+  {
+    id: 3,
+    name: '오뎅',
+    image: 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/babshim/20210525042622147ztkc.jpg',
+    rating: 4,
+  },
+  {
+    id: 4,
+    name: '감자',
+    image: 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/babshim/20210525042622147ztkc.jpg',
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: '순대',
+    image: 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/babshim/20210525042622147ztkc.jpg',
+    rating: 4,
+  },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <div>
+       {foodILike.map(dish => {
+         return <Food name={dish.name} pic={dish.image} />
+       })}
+   
+    </div> 
   );
 }
 
